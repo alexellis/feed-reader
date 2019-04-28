ad1fa5f9559df5f5275b197b31f46de24c6983f7
Alex Ellis <alexellis2@gmail.com>
2019-04-28 14:14:16 -0500

    Tester
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ reader/FunctionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input) {
+
+            var feed = FeedReader.Read("https://codehollow.com/feed");
+
+            // Console.WriteLine("Feed Title: " + feed.Title);
+            // Console.WriteLine("Feed Description: " + feed.Description);
+            // Console.WriteLine("Feed Image: " + feed.ImageUrl);
+            // ...
+            var items ="";
+            foreach(var item in feed.Items)
+            {
+                items = items + item.Title + " - " + item.Link+System.Environment.NewLine;
+            }
+
+            return items;
+        }
+    }
+}
